961a129552c16ff467c2da300ed4241fb7ed8f9a
Lavender Glaab <piggehperson@gmail.com>
2026-04-30 18:53:22 -0400

    Expose scale_gpoint

--- src/c/pdc/scale_pdc.c
+++ src/c/pdc/scale_pdc.c
@@ -4,7 +4,7 @@ typedef struct {
     float scale_factor;
 } ScalePointCBContext;
 
-static GPoint prv_scale_gpoint(GPoint point, float scale_factor) {
+GPoint scale_gpoint(GPoint point, float scale_factor) {
     GPoint scaled_point = GPoint(
         point.x *= scale_factor,
         point.y *= scale_factor
@@ -16,7 +16,7 @@ static bool prv_gdraw_command_scale_points(GDrawCommand *command, uint32_t index
   ScalePointCBContext *scale_ctx = context;
   // Move points
   for (int i = 0; i < gdraw_command_get_num_points(command); i++) {
-    gdraw_command_set_point(command, i, prv_scale_gpoint(gdraw_command_get_point(command, i), scale_ctx->scale_factor));
+    gdraw_command_set_point(command, i, scale_gpoint(gdraw_command_get_point(command, i), scale_ctx->scale_factor));
   }
 
   //Scale stroke width
--- src/c/pdc/scale_pdc.h
+++ src/c/pdc/scale_pdc.h
@@ -1,5 +1,7 @@
 #include <pebble.h>
 
+GPoint scale_gpoint(GPoint point, float scale_factor);
+
 void scale_draw_command_list(GDrawCommandList *list, float scale_factor);
 void scale_draw_command_image(GDrawCommandImage *image, float scale_factor);
 
