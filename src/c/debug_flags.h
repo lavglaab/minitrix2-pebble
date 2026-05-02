f1ec57ff3038b4e4789616e299b04561ec823588
Lavender Glaab <piggehperson@gmail.com>
2026-05-02 00:27:24 -0400

    Add debug flag for drawing a target under text

--- src/c/debug_flags.h
+++ src/c/debug_flags.h
@@ -1,6 +1,6 @@
 #include <pebble.h>
 
-// Here's a convenience function that wraps APP_LOG, and 
+// Here's a convenience function that wraps APP_LOG, and
 // adds a boolean condition. I can use this to enable and
 // disable various types of logging via defines in this file.
 #define LOG_IF_ENABLED(condition, level, fmt, args...) if (condition) { APP_LOG(level, fmt, ## args); }
@@ -9,4 +9,5 @@
 #define DEBUG_LOG_LIFECYCLE false // Life cycle events like window creation and function calls
 #define DEBUG_LOG_WEATHER true // Communication, processing, and storage relating to weather
 
-#define DEBUG_UI_DUMMYMODE false // Replace time and data display for store screenshots
+// #define DEBUG_UI_DUMMYMODE // Replace time and data display for store screenshots
+// #define DEBUG_UI_TEXT_LOCATIONS // Draw a target at the center point for each text block
--- src/c/text_drawing.c
+++ src/c/text_drawing.c
@@ -1,7 +1,14 @@
 #include "text_drawing.h"
+#include "debug_flags.h"
 
 // Draws text centered horizontally and vertically around the given point
 void text_draw_centered(GContext * ctx, const char * text, GFont const font, GPoint origin) {
+    #if defined(DEBUG_UI_TEXT_LOCATIONS)
+    // Draw a target at our origin
+    graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorWhite));
+    graphics_draw_circle(ctx, origin, 10);
+    #endif
+
     GRect box_max = GRect(0, 0, 200, 100); // unrealistically large constraint bounds, chosen arbitrarily
 
     // Figure out how large our text actually needs to be
@@ -12,11 +19,12 @@ void text_draw_centered(GContext * ctx, const char * text, GFont const font, GPo
     // Create the bounds we will draw our text within
     GRect bounds_adjusted = GRect(
         origin.x - (size_measured.w / 2),
-        origin.y - (size_measured.h / 2),
+        origin.y - (size_measured.h / 2) - (size_measured.h / 5), // text lines on pebble are taller than the actual type itself
         size_measured.w,
         size_measured.h
     );
 
     // And actually draw the text to the ctx
     graphics_draw_text(ctx, text, font, bounds_adjusted, GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
 }
