b9b1cf0f00765cb77a9d8ecd323b9079ca4f7c43
Lavender Glaab <piggehperson@gmail.com>
2026-05-13 15:30:34 -0400

    Add function to get desired complication font based on ContentSize pref

--- src/c/text_drawing.c
+++ src/c/text_drawing.c
@@ -1,6 +1,31 @@
 #include "text_drawing.h"
 #include "debug_flags.h"
 
+GFont text_get_complication_font() {
+    PreferredContentSize content_size = preferred_content_size();
+    switch (content_size) {
+        case PreferredContentSizeExtraLarge: // usually hidpi && large
+        return fonts_get_system_font(FONT_KEY_GOTHIC_28);
+            break;
+        case PreferredContentSizeLarge: // usually (hidpi && medium) || (regulardpi && large)
+            return fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+            break;
+        case PreferredContentSizeMedium: // usually (hidpi && small) || (regulardpi && medium)
+            // fall through, smallest size is 18
+        case PreferredContentSizeSmall: // usually regulardpi && small
+            // fall through, smallest size is 18
+        default:
+            return fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+            break;
+    }
+}
+
+GSize text_measure_simply(const char * text, GFont const font, GRect box_max) {
+    return graphics_text_layout_get_content_size(
+        text, font, box_max, GTextOverflowModeFill, GTextAlignmentCenter
+    );
+}
+
 // Draws text centered horizontally and vertically around the given point
 void text_draw_centered(GContext * ctx, const char * text, GFont const font, GPoint origin) {
     #if defined(DEBUG_UI_TEXT_LOCATIONS)
@@ -12,9 +37,7 @@ void text_draw_centered(GContext * ctx, const char * text, GFont const font, GPo
     GRect box_max = GRect(0, 0, 200, 100); // unrealistically large constraint bounds, chosen arbitrarily
 
     // Figure out how large our text actually needs to be
-    GSize size_measured = graphics_text_layout_get_content_size(
-        text, font, box_max, GTextOverflowModeFill, GTextAlignmentCenter
-    );
+    GSize size_measured = text_measure_simply(text, font, box_max);
 
     // Create the bounds we will draw our text within
     GRect bounds_adjusted = GRect(
--- src/c/text_drawing.h
+++ src/c/text_drawing.h
@@ -1,3 +1,4 @@
 #include <pebble.h>
-
+GFont text_get_complication_font();
+GSize text_measure_simply(const char * text, GFont const font, GRect box_max);
 void text_draw_centered(GContext * ctx, const char * text, GFont const font, GPoint origin);
